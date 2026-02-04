87a3e18e6d029e86c9b56b583759d904acfdb13e
kishimai <ajayharris.codes@gmail.com>
2026-02-04 15:24:32 +0800

    Started Setting Up

---
+++ app/routes/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function RoutesScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Routes will appear here</Text>
+        </View>
+    );
+}
